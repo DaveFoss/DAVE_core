b69423d2c49beee65195e5de3e57d94ab769b328
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2024-08-23 16:17:14 +0200

    count up version number and updated changelog

--- src/dave_core/__init__.py
+++ src/dave_core/__init__.py
@@ -2,7 +2,7 @@
 # Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 from dave_core.archiv_io import *
 from dave_core.create import *

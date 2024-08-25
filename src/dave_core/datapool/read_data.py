ed615676b01b626dfc7c9ca2d9375a644c1f9a1c
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2024-08-25 02:34:08 +0200

    deleted data from datapool to reduce repo size and added function to download data to datapool if missing

--- src/dave_core/datapool/read_data.py
+++ src/dave_core/datapool/read_data.py
@@ -2,6 +2,9 @@
 # Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+import os
+
+import requests
 from geopandas import GeoDataFrame
 from geopandas import points_from_xy
 from pandas import HDFStore
@@ -14,6 +17,24 @@ from xmlschema import XMLSchema
 
 from dave_core.settings import dave_settings
 from dave_core.toolbox import get_data_path
+
+
+def download_data(filename):
+    """
+    Download data from DAVE_data ownCloud storage
+    """
+    url = f"https://owncloud.fraunhofer.de/index.php/s/McrHKZ62ci0FxCN/download?path=%2F&files={filename}"
+    file_path = os.path.join(get_data_path(dirname="data"), filename)
+    r = requests.get(url, stream=True)
+    if r.ok:
+        with open(file_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:
+        print(f"Download failed: status code {r.status_code}\n{r.text}")
 
 
 def read_postal():
@@ -29,13 +50,19 @@ def read_postal():
 
          postal = data.read_postal()
     """
+    # check if data is existing in datapool otherwise download it
+    filename = "postalcodesger.h5"
+    if not os.path.isfile(get_data_path(filename, "data")):
+        download_data(filename)
     # get data from datapool
-    postalger = read_hdf(get_data_path("postalcodesger.h5", "data"))
+    postalger = read_hdf(get_data_path(filename, "data"))
     # convert geometry
     postalger["geometry"] = postalger.geometry.apply(loads)
     postalger = GeoDataFrame(postalger, crs=dave_settings["crs_main"])
     # read meta data
-    meta_data = read_excel(get_data_path("postalcodesger_meta.xlsx", "data"), sheet_name=None)
+    meta_data = read_excel(
+        get_data_path("postalcodesger_meta.xlsx", "data"), sheet_name=None
+    )
     return postalger, meta_data
 
 
@@ -52,11 +79,21 @@ def read_federal_states():
 
          federal = data.read_federal_states()
     """
-    federalstatesger = read_hdf(get_data_path("federalstatesger.h5", "data"))
+    # check if data is existing in datapool otherwise download it
+    filename = "federalstatesger.h5"
+    if not os.path.isfile(get_data_path(filename, "data")):
+        download_data(filename)
+    # get data from datapool
+    federalstatesger = read_hdf(get_data_path(filename, "data"))
+    # convert geometry
     federalstatesger["geometry"] = federalstatesger.geometry.apply(loads)
-    federalstatesger = GeoDataFrame(federalstatesger, crs=dave_settings["crs_main"])
+    federalstatesger = GeoDataFrame(
+        federalstatesger, crs=dave_settings["crs_main"]
+    )
     # read meta data
-    meta_data = read_excel(get_data_path("federalstatesger_meta.xlsx", "data"), sheet_name=None)
+    meta_data = read_excel(
+        get_data_path("federalstatesger_meta.xlsx", "data"), sheet_name=None
+    )
     return federalstatesger, meta_data
 
 
@@ -72,20 +109,39 @@ def read_nuts_regions(year):
 
          nuts = data.read_nuts_regions()
     """
+    # check if data is existing in datapool otherwise download it
+    filename = "nuts_regions.h5"
+    if not os.path.isfile(get_data_path(filename, "data")):
+        download_data(filename)
+    # get data from datapool
     if year == "2013":
-        nuts_regions = read_hdf(get_data_path("nuts_regions.h5", "data"), key="/nuts_2013")
+        nuts_regions = read_hdf(
+            get_data_path(filename, "data"), key="/nuts_2013"
+        )
         nuts_regions["geometry"] = nuts_regions.geometry.apply(loads)
-        nuts_regions = GeoDataFrame(nuts_regions, crs=dave_settings["crs_main"])
+        nuts_regions = GeoDataFrame(
+            nuts_regions, crs=dave_settings["crs_main"]
+        )
     elif year == "2016":
-        nuts_regions = read_hdf(get_data_path("nuts_regions.h5", "data"), key="/nuts_2016")
+        nuts_regions = read_hdf(
+            get_data_path(filename, "data"), key="/nuts_2016"
+        )
         nuts_regions["geometry"] = nuts_regions.geometry.apply(loads)
-        nuts_regions = GeoDataFrame(nuts_regions, crs=dave_settings["crs_main"])
+        nuts_regions = GeoDataFrame(
+            nuts_regions, crs=dave_settings["crs_main"]
+        )
     elif year == "2021":
-        nuts_regions = read_hdf(get_data_path("nuts_regions.h5", "data"), key="/nuts_2021")
+        nuts_regions = read_hdf(
+            get_data_path(filename, "data"), key="/nuts_2021"
+        )
         nuts_regions["geometry"] = nuts_regions.geometry.apply(loads)
-        nuts_regions = GeoDataFrame(nuts_regions, crs=dave_settings["crs_main"])
+        nuts_regions = GeoDataFrame(
+            nuts_regions, crs=dave_settings["crs_main"]
+        )
     # read meta data
-    meta_data = read_excel(get_data_path("nuts_regions_meta.xlsx", "data"), sheet_name=None)
+    meta_data = read_excel(
+        get_data_path("nuts_regions_meta.xlsx", "data"), sheet_name=None
+    )
     return nuts_regions, meta_data
 
 
@@ -102,9 +158,13 @@ def read_household_consumption():
 
          household_consumption = data.read_household_consumption()
     """
+    # check if data is existing in datapool otherwise download it
+    filename = "household_power_consumption.h5"
+    if not os.path.isfile(get_data_path(filename, "data")):
+        download_data(filename)
     # --- get data from datapool
     # read data
-    consumption_data = HDFStore(get_data_path("household_power_consumption.h5", "data"))
+    consumption_data = HDFStore(get_data_path(filename, "data"))
     # consumption avarage
     household_consumptions = consumption_data.get("/household_consumptions")
     household_sizes = consumption_data.get("/household_sizes")
@@ -136,9 +196,13 @@ def read_scigridgas_iggielgn():
 
          scigridgas_iggielgn = data.read_scigridgas_iggielgn()
     """
+    # check if data is existing in datapool otherwise download it
+    filename = "scigridgas_iggielgn.h5"
+    if not os.path.isfile(get_data_path(filename, "data")):
+        download_data(filename)
     # --- get data from datapool
     # read data
-    iggielgn_data = HDFStore(get_data_path("scigridgas_iggielgn.h5", "data"))
+    iggielgn_data = HDFStore(get_data_path(filename, "data"))
     # border_points
     border_points = iggielgn_data.get("/scigridgas_iggielgn_border_points")
     border_points = GeoDataFrame(
@@ -178,7 +242,10 @@ def read_scigridgas_iggielgn():
     pipe_segments = iggielgn_data.get("/scigridgas_iggielgn_pipe_segments")
     pipe_segments.lat = pipe_segments.lat.apply(eval)
     pipe_segments.long = pipe_segments.long.apply(eval)
-    geometry = [LineString(list(zip(pipe.long, pipe.lat))) for i, pipe in pipe_segments.iterrows()]
+    geometry = [
+        LineString(list(zip(pipe.long, pipe.lat)))
+        for i, pipe in pipe_segments.iterrows()
+    ]
     pipe_segments = GeoDataFrame(
         pipe_segments, geometry=Series(geometry), crs=dave_settings["crs_main"]
     )
@@ -210,14 +277,18 @@ def read_scigridgas_iggielgn():
         "storages": storages,
     }
     # read meta data
-    meta_data = read_excel(get_data_path("scigridgas_iggielgn_meta.xlsx", "data"), sheet_name=None)
+    meta_data = read_excel(
+        get_data_path("scigridgas_iggielgn_meta.xlsx", "data"), sheet_name=None
+    )
     return storage_data, meta_data
 
 
 def read_gaslib():
     # read data from datapool
     schema = XMLSchema(get_data_path("gaslib/Gas.xsd", "data"))
-    gaslib_dict = schema.to_dict(get_data_path("gaslib/GasLib-582-v2.net", "data"))
+    gaslib_dict = schema.to_dict(
+        get_data_path("gaslib/GasLib-582-v2.net", "data")
+    )
     # create data dictionary
     gaslib_data = {
         "nodes": gaslib_dict["framework:nodes"],

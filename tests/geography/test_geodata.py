45cf830173b84c69ccf9f81703474c743f6b7dcd
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2025-08-22 15:45:44 +0200

    added test for geodata and disable the lv topo test

--- tests/geography/test_geodata.py
+++ tests/geography/test_geodata.py
@@ -4,16 +4,20 @@
 # Copyright (c) 2024-2025 DAVE_core contributors
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
+from pathlib import Path
 
+import geopandas as gpd
 import pytest
 
+from dave_core.create import create_grid
 
-def test_lv_topo():
+
+def test_geodata():
     """
     This test checks the low voltage topology generation based on an own polygon
     for the grid area.
     """
-    """
+
     # test own polygon
     # Hertingshausen is a part from the Town Baunatal. (ca. 500 relevant Buildings)
     own_area_path = Path(__file__).resolve().parents[2] / "examples" / "hertingshausen.geojson"
@@ -21,13 +25,7 @@ def test_lv_topo():
     # run topology generation
     grid_data = create_grid(
         own_area=own_area_polygon,
-        # grid level parameters
-        power_levels=["lv"],
-        # power grid components
-        transformers=False,
-        renewable_powerplants=False,
-        conventional_powerplants=False,
-        loads=False,
+        geodata=["ALL"],
         save_data=False,
     )
     # check results
@@ -35,16 +33,15 @@ def test_lv_topo():
     assert not grid_data.buildings.commercial.empty, "No commercial building data were collected."
     assert not grid_data.buildings.residential.empty, "No residential building data were collected."
     assert not grid_data.roads.roads.empty, "No road data data were collected."
-    assert (
-        not grid_data.roads.road_junctions.empty
-    ), "No road junctions was calculated and returned."
     assert not grid_data.landuse.empty, "No landuse data data were collected."
-    assert not grid_data.lv_data.lv_nodes.empty, "No low voltage nodes were generated."
-    assert not grid_data.lv_data.lv_lines.empty, "No low voltage lines were generated."
-    assert (
-        not grid_data.components_power.substations.mv_lv.empty
-    ), "No mv/lv substation data were collected."
-    """
+
+
+def test_road_junctions():
+    pass
+
+    # assert (
+    #     not grid_data.roads.road_junctions.empty
+    # ), "No road junctions was calculated and returned."
 
 
 if __name__ == "__main__":

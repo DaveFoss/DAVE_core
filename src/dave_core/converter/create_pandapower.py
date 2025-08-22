8dbb0780e8a319e5b7f0ec4d79057755580204f9
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2025-08-22 14:51:57 +0200

    updated copyright

--- src/dave_core/converter/create_pandapower.py
+++ src/dave_core/converter/create_pandapower.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2022-2024 by Fraunhofer Institute for Energy Economics and Energy System Technology (IEE)
-# Kassel and individual contributors (see AUTHORS file for details). All rights reserved.
+# Kassel and individual contributors (see AUTHORS file for details).
+# All rights reserved.
+# Copyright (c) 2024-2025 DAVE_core contributors
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
 
 from pandapower import __version__ as pp_version
 from pandapower import available_std_types

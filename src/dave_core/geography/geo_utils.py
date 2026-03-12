713776e50d32f8881fa9626602f767272219e6da
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2026-03-12 16:33:03 +0100

    added generate_road_endings function

--- src/dave_core/geography/geo_utils.py
+++ src/dave_core/geography/geo_utils.py
@@ -6,7 +6,11 @@
 
 
 from dask_geopandas import from_geopandas
+from geopandas import GeoDataFrame
+from geopandas import GeoSeries
+from pandas import concat
 from shapely import union_all
+from shapely.geometry import Point
 from shapely.ops import nearest_points
 
 from dave_core.progressbar import create_tqdm_dask
@@ -33,3 +37,47 @@ def nearest_road_points(points, roads):
         return points_dask.apply(
             lambda x: nearest_points(x, multiline_roads)[1], meta=points_dask
         ).compute()
+
+
+def generate_road_endings(relevant_roads, nodes):
+    """
+    This function filters all road endings which do not correspond to given \
+        nodes
+
+    INPUT:
+        **relevant_roads** (dict) - roads that should be considered
+        **nodes** (dict) - existing nodes to avoid duplication
+
+    OUTPUT:
+        **road_endings_rel** (GeoDataFrame) - all relevant road endings
+    """
+    # extract roads ending points
+    relevant_roads_dask = from_geopandas(
+        relevant_roads.geometry, npartitions=dave_settings["cpu_number"]
+    )
+    roads_from_points = relevant_roads_dask.apply(
+        lambda x: Point(x.coords[0]),
+        meta=relevant_roads_dask,
+    ).compute()
+    roads_to_points = relevant_roads_dask.apply(
+        lambda x: Point(x.coords[-1]),
+        meta=relevant_roads_dask,
+    ).compute()
+    road_endings = GeoDataFrame(
+        geometry=GeoSeries(concat([roads_from_points, roads_to_points], ignore_index=True))
+    )
+    # filter all road endings which are not close to existing nodes to avoid duplicates
+    road_endings_dask = from_geopandas(
+        road_endings.geometry, npartitions=dave_settings["cpu_number"]
+    )
+    nodes_buffer = nodes.buffer(1e-8).union_all()
+    road_endings_near_nodes = road_endings_dask.apply(
+        lambda x, nodes_buffer=nodes_buffer: x.within(nodes_buffer),
+        meta=road_endings_dask,
+    ).compute()
+    road_endings_rel = road_endings[~road_endings_near_nodes]
+    # adjust relevant data
+    road_endings_rel.reset_index(drop=True, inplace=True)
+    road_endings_rel["node_type"] = "road_ending"
+    road_endings_rel["source"] = "dave internal"
+    return road_endings_rel

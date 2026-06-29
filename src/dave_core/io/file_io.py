d93d9ab8f08602c1c75d1fe1ae991afe10122bfc
Tobias Banze <tobias.banze@iee.fraunhofer.de>
2026-06-29 13:46:25 +0200

    add readibility checks for from json function

--- src/dave_core/io/file_io.py
+++ src/dave_core/io/file_io.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022-2024 by Fraunhofer Institute for Energy Economics and Energy System Technology (IEE)
 # Kassel and individual contributors (see AUTHORS file for details).
 # All rights reserved.
-# Copyright (c) 2024-2025 DAVE_core contributors
+# Copyright (c) 2024-2026 DAVE_core contributors
 # Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
 
@@ -9,6 +9,7 @@ import json
 from copy import deepcopy
 from functools import partial
 from json import dumps as json_dumps
+from json import load as json_load
 from json import loads as json_loads
 from pathlib import Path
 
@@ -55,10 +56,36 @@ def safe_to_json(net, filename, encryption_key=None, store_index_names=True):
             f.write(json_string)
 
 
-# pp_io.to_json = safe_to_json
+pp_io.to_json = safe_to_json
 
 
 # --- JSON
+def find_empty_strings(obj, path="root"):
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            if value == "":
+                print(f"Leerer String: {path}.{key}")
+
+            find_empty_strings(value, f"{path}.{key}")
+
+    elif isinstance(obj, list):
+        for i, value in enumerate(obj):
+            find_empty_strings(value, f"{path}[{i}]")
+
+
+def check_json_readability(file_path):
+    """
+    This function checks the json file
+
+    """
+    # check general readability of the file
+    with Path(file_path).open() as f:
+        data = json_load(f)
+
+    # find empty string
+    find_empty_strings(data)
+
+
 def from_json(file_path, encryption_key=None):
     """
     Load a dave dataset from a JSON file.
@@ -72,6 +99,9 @@ def from_json(file_path, encryption_key=None):
         **file** (json) - the DAVE dataset in JSON format
 
     """
+    # check general readability:
+    check_json_readability(file_path)
+    # read file
     if hasattr(file_path, "read"):
         json_string = file_path.read()
     elif not Path(file_path).is_file():
@@ -80,8 +110,17 @@ def from_json(file_path, encryption_key=None):
         with Path(file_path).open("r") as file:
             json_string = file.read()
     # check if it is a json string in DAVE, pandapower or pandapipes structure
-    json_type = json_loads(json_string)["_module"]
-    if json_type in [
+    json_type = json_loads(json_string)
+    # fix missing dave structure
+    if "_module" not in json_type.keys():
+        # create dave structure dict
+        json_type = {
+            "_module": "dave_core.dave_structure",
+            "_class": "davestructure",
+            "_object": json_type,
+        }
+        # Problem: die darunter liegenden daten sind nicht im DAVE-Structure ( bsp. Buildings)
+    if json_type["_module"] in [
         "dave_core.dave_structure",
     ]:
         # read data from json file
@@ -92,10 +131,10 @@ def from_json(file_path, encryption_key=None):
         for key in grid_data.keys():
             grid_data[key] = deepcopy(data[key])
         return grid_data
-    elif json_type == "pandapower.auxiliary":
+    elif json_type["_module"] == "pandapower.auxiliary":
         print("A pandapower network is given as input and will be convertert in pandapower format")
         return from_json_pp(file_path)
-    elif json_type == "ppi":
+    elif json_type["_module"] == "ppi":
         print("A pandapipes network is given as input and will be convertert in pandapipes format")
         return from_json_ppi(file_path)
     else:
@@ -111,7 +150,7 @@ def from_json_string(json_string, encryption_key=None):
         **encrytion_key** (string, None) - If given, the DAVE dataset is stored as an encrypted \
             json string
     OUTPUT:
-        **test** (json) - the DAVE dataset in JSON format
+        **dataset** (json) - the DAVE dataset in JSON format
     """
     if encryption_key is not None:
         json_string = decrypt_string(json_string, encryption_key)
